bae388b460bd7541732b050c8328faade9443303
CapolongoDa <Daniele.Capolongo@emea.nttdata.com>
2023-05-02 16:15:56 +0200

    Added jUnit test

--- src/test/java/it/pagopa/pn/radd/bff/rest/NotificationInquiryControllerTest.java
+++ src/test/java/it/pagopa/pn/radd/bff/rest/NotificationInquiryControllerTest.java
@@ -1,27 +1,14 @@
 package it.pagopa.pn.radd.bff.rest;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import it.pagopa.pn.radd.bff.log.RaddRequestDecorator;
-import it.pagopa.pn.radd.bff.log.RaddResponseDecorator;
-import it.pagopa.pn.radd.bff.log.RaddWebExchangeDecorator;
 import it.pagopa.pn.radd.bff.rest.v1.dto.*;
 import it.pagopa.pn.radd.bff.service.NotificationInquiryService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
-import org.springframework.mock.http.server.reactive.MockServerHttpResponse;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.adapter.DefaultServerWebExchange;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 import reactor.test.StepVerifier;
@@ -29,114 +16,186 @@ import reactor.test.StepVerifier;
 import java.util.Date;
 import java.util.List;
 
-@ContextConfiguration(classes = {NotificationInquiryController.class})
-@ExtendWith(SpringExtension.class)
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith (SpringExtension.class)
 class NotificationInquiryControllerTest {
-    @Autowired
-    private NotificationInquiryController notificationInquiryController;
+	@InjectMocks
+	private NotificationInquiryController notificationInquiryController;
 
-    @MockBean
-    private NotificationInquiryService notificationInquiryService;
+	@Mock
+	private NotificationInquiryService notificationInquiryService;
 
-    @Mock
-    ServerWebExchange serverWebExchange;
+	@Mock
+	ServerWebExchange serverWebExchange;
 
-    @MockBean
-    private Scheduler scheduler;
+	@Mock
+	private Scheduler scheduler;
 
-    /**
-     * Method under test: {@link NotificationInquiryController#getActPracticesByInternalId(String, Mono, ServerWebExchange)}
-     */
-    @Test
-    void testGetActPracticesByInternalId3() {
-        when(notificationInquiryService.getActPracticesByInternalId(Mockito.<String>any(),
-                Mockito.<Mono<FilterRequest>>any())).thenReturn(mock(Mono.class));
-        Mono<FilterRequest> filterRequest = mock(Mono.class);
-        DefaultServerWebExchange exchange = mock(DefaultServerWebExchange.class);
-        when(exchange.getResponse())
-                .thenReturn(new RaddResponseDecorator(new RaddResponseDecorator(new RaddResponseDecorator(
-                        new RaddResponseDecorator(new RaddResponseDecorator(new MockServerHttpResponse()))))));
-        when(exchange.getRequest())
-                .thenReturn(
-                        new RaddRequestDecorator(new RaddRequestDecorator(new RaddRequestDecorator(
-                                new RaddRequestDecorator(
-                                        new RaddRequestDecorator(mock(ServerHttpRequestDecorator.class), "Masked URI"), "Masked URI"),
-                                "Masked URI"), "Masked URI"), "Masked URI"));
-        notificationInquiryController.getActPracticesByInternalId("42", filterRequest,
-                new RaddWebExchangeDecorator(new RaddWebExchangeDecorator(
-                        new RaddWebExchangeDecorator(new RaddWebExchangeDecorator(exchange, "Masked URI"), "Masked URI"),
-                        "Masked URI"), "Masked URI"));
-        verify(notificationInquiryService).getActPracticesByInternalId(Mockito.<String>any(),
-                Mockito.<Mono<FilterRequest>>any());
-        verify(exchange).getRequest();
-        verify(exchange).getResponse();
-    }
+	/**
+	 * Method under test: {@link NotificationInquiryController#getActPracticesByInternalId(String, Mono, ServerWebExchange)}
+	 */
+	@Test
+	void testGetActPracticesByInternalId () {
+		Mono<FilterRequest> filterRequest = mock(Mono.class);
 
-    @Test
-    void testGetActPracticesByIun() {
-        OperationsResponse operationsResponse = new OperationsResponse();
+		OperationsActDetailsResponse operationsActDetailsResponse = mock(OperationsActDetailsResponse.class);
+		OperationActDetailResponse operationActDetailResponse = mock(OperationActDetailResponse.class);
+		operationsActDetailsResponse.setElements(List.of(operationActDetailResponse));
+		operationsActDetailsResponse.setResult(true);
 
-        OperationsDetailsResponse operationsDetailsResponse = new OperationsDetailsResponse();
-        operationsDetailsResponse.setOperationId("operationId");
-        operationsDetailsResponse.setOperationStatus("operationStatus");
-        operationsDetailsResponse.setFileKey("fileKey");
-        operationsDetailsResponse.setOperationType("operationType");
-        operationsDetailsResponse.setQrCode("qrCode");
-        operationsDetailsResponse.setErrorReason("errorReason");
-        operationsDetailsResponse.setRecipientType("recipientType");
-        operationsDetailsResponse.setUid("uid");
-        operationsDetailsResponse.setOperationEndDate(new Date(System.currentTimeMillis() + 1000000L));
-        operationsDetailsResponse.setOperationStartDate(new Date(System.currentTimeMillis()));
-        operationsDetailsResponse.setIun("iun");
+		OperationResponseStatus operationResponseStatus = new OperationResponseStatus();
+		operationResponseStatus.setCode(OperationResponseStatus.CodeEnum.NUMBER_0);
+		operationResponseStatus.setMessage("message");
 
-        operationsResponse.setOperations(List.of(operationsDetailsResponse));
-        operationsResponse.setResult(true);
+		operationsActDetailsResponse.setStatus(operationResponseStatus);
 
-        OperationResponseStatus operationResponseStatus = new OperationResponseStatus();
-        operationResponseStatus.setCode(OperationResponseStatus.CodeEnum.NUMBER_0);
-        operationResponseStatus.setMessage("message");
+		when(notificationInquiryService.getActPracticesByInternalId("taxId",filterRequest))
+				.thenReturn(Mono.just(operationsActDetailsResponse));
 
-        operationsResponse.setStatus(operationResponseStatus);
+		StepVerifier.create(notificationInquiryController.getActPracticesByInternalId("taxId",filterRequest ,serverWebExchange))
+				.expectNext(ResponseEntity.ok().body(operationsActDetailsResponse));
+	}
+	@Test
+	void testGetAorPracticesByInternalId () {
+		Mono<FilterRequest> filterRequest = mock(Mono.class);
 
-        when(notificationInquiryService.getActPracticesByIun("iun"))
-                .thenReturn(Mono.just(operationsResponse));
+		OperationsAorDetailsResponse operationsAorDetailsResponse = mock(OperationsAorDetailsResponse.class);
+		OperationAorDetailResponse operationAorDetailResponse = mock(OperationAorDetailResponse.class);
+		operationsAorDetailsResponse.setElements(List.of(operationAorDetailResponse));
+		operationsAorDetailsResponse.setResult(true);
 
-        StepVerifier.create(notificationInquiryController.getActPracticesByIun("iun", serverWebExchange))
-                .expectNext(ResponseEntity.ok().body(operationsResponse));
-    }
+		OperationResponseStatus operationResponseStatus = new OperationResponseStatus();
+		operationResponseStatus.setCode(OperationResponseStatus.CodeEnum.NUMBER_0);
+		operationResponseStatus.setMessage("message");
 
-    @Test
-    void testGetAorPracticesByIun() {
-        OperationsResponse operationsResponse = new OperationsResponse();
+		operationsAorDetailsResponse.setStatus(operationResponseStatus);
 
-        OperationsDetailsResponse operationsDetailsResponse = new OperationsDetailsResponse();
-        operationsDetailsResponse.setOperationId("operationId");
-        operationsDetailsResponse.setOperationStatus("operationStatus");
-        operationsDetailsResponse.setFileKey("fileKey");
-        operationsDetailsResponse.setOperationType("operationType");
-        operationsDetailsResponse.setQrCode("qrCode");
-        operationsDetailsResponse.setErrorReason("errorReason");
-        operationsDetailsResponse.setRecipientType("recipientType");
-        operationsDetailsResponse.setUid("uid");
-        operationsDetailsResponse.setOperationEndDate(new Date(System.currentTimeMillis() + 1000000L));
-        operationsDetailsResponse.setOperationStartDate(new Date(System.currentTimeMillis()));
-        operationsDetailsResponse.setIun("iun");
+		when(notificationInquiryService.getAorPracticesByInternalId("taxId",filterRequest))
+				.thenReturn(Mono.just(operationsAorDetailsResponse));
 
-        operationsResponse.setOperations(List.of(operationsDetailsResponse));
-        operationsResponse.setResult(true);
+		StepVerifier.create(notificationInquiryController.getAorPracticesByInternalId("taxId",filterRequest ,serverWebExchange))
+				.expectNext(ResponseEntity.ok().body(operationsAorDetailsResponse));
+	}
+	@Test
+	void testGetActPracticesByIun () {
+		OperationsResponse operationsResponse = new OperationsResponse();
 
-        OperationResponseStatus operationResponseStatus = new OperationResponseStatus();
-        operationResponseStatus.setCode(OperationResponseStatus.CodeEnum.NUMBER_0);
-        operationResponseStatus.setMessage("message");
+		OperationsDetailsResponse operationsDetailsResponse = new OperationsDetailsResponse();
+		operationsDetailsResponse.setOperationId("operationId");
+		operationsDetailsResponse.setOperationStatus("operationStatus");
+		operationsDetailsResponse.setFileKey("fileKey");
+		operationsDetailsResponse.setOperationType("operationType");
+		operationsDetailsResponse.setQrCode("qrCode");
+		operationsDetailsResponse.setErrorReason("errorReason");
+		operationsDetailsResponse.setRecipientType("recipientType");
+		operationsDetailsResponse.setUid("uid");
+		operationsDetailsResponse.setOperationEndDate(new Date(System.currentTimeMillis() + 1000000L));
+		operationsDetailsResponse.setOperationStartDate(new Date(System.currentTimeMillis()));
+		operationsDetailsResponse.setIun("iun");
 
-        operationsResponse.setStatus(operationResponseStatus);
+		operationsResponse.setOperations(List.of(operationsDetailsResponse));
+		operationsResponse.setResult(true);
 
-        when(notificationInquiryService.getAorPracticesByIun("iun"))
-                .thenReturn(Mono.just(operationsResponse));
+		OperationResponseStatus operationResponseStatus = new OperationResponseStatus();
+		operationResponseStatus.setCode(OperationResponseStatus.CodeEnum.NUMBER_0);
+		operationResponseStatus.setMessage("message");
 
-        StepVerifier.create(notificationInquiryController.getAorPracticesByIun("iun", serverWebExchange))
-                .expectNext(ResponseEntity.ok().body(operationsResponse));
-    }
+		operationsResponse.setStatus(operationResponseStatus);
+
+		when(notificationInquiryService.getActPracticesByIun("iun"))
+				.thenReturn(Mono.just(operationsResponse));
+
+		StepVerifier.create(notificationInquiryController.getActPracticesByIun("iun", serverWebExchange))
+				.expectNext(ResponseEntity.ok().body(operationsResponse));
+	}
+	@Test
+	void testGetActTransactionByOperationId () {
+		OperationActResponse operationActResponse = new OperationActResponse();
+
+		OperationActDetailResponse operationActDetailResponse = new OperationActDetailResponse();
+
+		operationActDetailResponse.setOperationId("operationId");
+		operationActDetailResponse.setOperationStatus("operationStatus");
+		operationActDetailResponse.setOperationType("operationType");
+		operationActDetailResponse.setIun("Iun");
+		operationActDetailResponse.setFileKey("fileKey");
+		operationActDetailResponse.setQrCode("qrCode");
+		operationActDetailResponse.setRecipientTaxId("recipientTaxId");
+		operationActDetailResponse.setErrorReason("errorReason");
+		operationActDetailResponse.setRecipientType("recipientType");
+		operationActDetailResponse.setUid("uid");
+		operationActDetailResponse.setOperationEndDate(new Date(System.currentTimeMillis() + 1000000L));
+		operationActDetailResponse.setOperationStartDate(new Date(System.currentTimeMillis()));
+
+		operationActResponse.setElement(operationActDetailResponse);
+
+		OperationResponseStatus operationResponseStatus = new OperationResponseStatus();
+		operationResponseStatus.setCode(OperationResponseStatus.CodeEnum.NUMBER_0);
+		operationResponseStatus.setMessage("message");
+
+		operationActResponse.setStatus(operationResponseStatus);
+
+		when(notificationInquiryService.getActTransactionByOperationId("operationId"))
+				.thenReturn(Mono.just(operationActResponse));
+
+		StepVerifier.create(notificationInquiryController.getActTransactionByOperationId("operationId", serverWebExchange))
+				.expectNext(ResponseEntity.ok().body(operationActResponse));
+	}
+	@Test
+	void testGetAorTransactionByOperationId () {
+		OperationAorResponse operationAorResponse = new OperationAorResponse();
+
+		OperationAorDetailResponse operationAorDetailResponse = mock(OperationAorDetailResponse.class);
+
+		operationAorResponse.setElement(operationAorDetailResponse);
+
+		OperationResponseStatus operationResponseStatus = new OperationResponseStatus();
+		operationResponseStatus.setCode(OperationResponseStatus.CodeEnum.NUMBER_0);
+		operationResponseStatus.setMessage("message");
+
+		operationAorResponse.setStatus(operationResponseStatus);
+
+		when(notificationInquiryService.getAorTransactionByOperationId("operationId"))
+				.thenReturn(Mono.just(operationAorResponse));
+
+		StepVerifier.create(notificationInquiryController.getAorTransactionByOperationId("operationId", serverWebExchange))
+				.expectNext(ResponseEntity.ok().body(operationAorResponse));
+	}
+	@Test
+	void testGetAorPracticesByIun () {
+		OperationsResponse operationsResponse = new OperationsResponse();
+
+		OperationsDetailsResponse operationsDetailsResponse = new OperationsDetailsResponse();
+		operationsDetailsResponse.setOperationId("operationId");
+		operationsDetailsResponse.setOperationStatus("operationStatus");
+		operationsDetailsResponse.setFileKey("fileKey");
+		operationsDetailsResponse.setOperationType("operationType");
+		operationsDetailsResponse.setQrCode("qrCode");
+		operationsDetailsResponse.setErrorReason("errorReason");
+		operationsDetailsResponse.setRecipientType("recipientType");
+		operationsDetailsResponse.setUid("uid");
+		operationsDetailsResponse.setOperationEndDate(new Date(System.currentTimeMillis() + 1000000L));
+		operationsDetailsResponse.setOperationStartDate(new Date(System.currentTimeMillis()));
+		operationsDetailsResponse.setIun("iun");
+
+		operationsResponse.setOperations(List.of(operationsDetailsResponse));
+		operationsResponse.setResult(true);
+
+		OperationResponseStatus operationResponseStatus = new OperationResponseStatus();
+		operationResponseStatus.setCode(OperationResponseStatus.CodeEnum.NUMBER_0);
+		operationResponseStatus.setMessage("message");
+
+		operationsResponse.setStatus(operationResponseStatus);
+
+		when(notificationInquiryService.getAorPracticesByIun("iun"))
+				.thenReturn(Mono.just(operationsResponse));
+
+		StepVerifier.create(notificationInquiryController.getAorPracticesByIun("iun", serverWebExchange))
+				.expectNext(ResponseEntity.ok().body(operationsResponse));
+	}
 
 }
 
---
+++ src/test/java/it/pagopa/pn/radd/bff/service/NotificationInquiryServiceTest.java
@@ -0,0 +1,108 @@
+package it.pagopa.pn.radd.bff.service;
+
+import it.pagopa.pn.radd.bff.client.PnRaddFsuClient;
+import it.pagopa.pn.radd.bff.converter.NotificationInquiryConverter;
+import it.pagopa.pn.radd.bff.msclient.generated.radd.fsu.v1.dto.OperationsActDetailsResponseDto;
+import it.pagopa.pn.radd.bff.msclient.generated.radd.fsu.v1.dto.OperationsAorDetailsResponseDto;
+import it.pagopa.pn.radd.bff.rest.v1.dto.FilterRequest;
+import it.pagopa.pn.radd.bff.rest.v1.dto.OperationsActDetailsResponse;
+import it.pagopa.pn.radd.bff.rest.v1.dto.OperationsAorDetailsResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
+
+@ExtendWith (SpringExtension.class)
+class NotificationInquiryServiceTest {
+	@Mock
+	private NotificationInquiryConverter notificationInquiryConverter;
+
+	@Mock
+	private PnRaddFsuClient pnRaddFsuClient;
+
+	@InjectMocks
+	private NotificationInquiryService notificationInquiryService;
+
+
+
+	@Test
+	void testGetActPracticesByInternalId(){
+		when(pnRaddFsuClient.getActPracticesByInternalId(any(),any()))
+				.thenReturn((Mono<OperationsActDetailsResponseDto>) mock(Mono.class));
+		OperationsActDetailsResponse operationsActDetailsResponse = mock(OperationsActDetailsResponse.class);
+		when(notificationInquiryConverter.operationsActDetailsDtoToResponse(any()))
+				.thenReturn(operationsActDetailsResponse);
+		FilterRequest filterRequest = mock(FilterRequest.class);
+		StepVerifier.create(notificationInquiryService.getActPracticesByInternalId("taxId",Mono.just(filterRequest)))
+				.expectNext(operationsActDetailsResponse);
+	}
+
+	@Test
+	void testGetAorPracticesByInternalId(){
+		when(pnRaddFsuClient.getAorPracticesByInternalId(any(),any()))
+				.thenReturn((Mono<OperationsAorDetailsResponseDto>) mock(Mono.class));
+		OperationsAorDetailsResponse operationsAorDetailsResponse = mock(OperationsAorDetailsResponse.class);
+		when(notificationInquiryConverter.operationsAorDetailsDtoToResponse(any()))
+				.thenReturn(operationsAorDetailsResponse);
+		FilterRequest filterRequest = mock(FilterRequest.class);
+		StepVerifier.create(notificationInquiryService.getAorPracticesByInternalId("taxId",Mono.just(filterRequest)))
+				.expectNext(operationsAorDetailsResponse);
+	}
+
+	/**
+	 * Method under test: {@link NotificationInquiryService#getActPracticesByIun(String)}
+	 */
+	@Test
+	void testGetActPracticesByIun () {
+		when(pnRaddFsuClient.getActPracticesByIun(any()))
+				.thenReturn(mock(Mono.class));
+		StepVerifier.create(notificationInquiryService.getActPracticesByIun("Iun"));
+	}
+
+	/**
+	 * Method under test: {@link NotificationInquiryService#getActTransactionByOperationId(String)}
+	 */
+	@Test
+	void testGetActTransactionByOperationId () {
+		// Arrange
+		when(pnRaddFsuClient.getActTransactionByOperationId(any())).thenReturn(mock(Mono.class));
+
+		// Act
+		notificationInquiryService.getActTransactionByOperationId("42");
+
+		// Assert
+		verify(pnRaddFsuClient).getActTransactionByOperationId(any());
+	}
+
+	/**
+	 * Method under test: {@link NotificationInquiryService#getAorPracticesByIun(String)}
+	 */
+	@Test
+	void testGetAorPracticesByIun () {
+		when(pnRaddFsuClient.getAorPracticesByIun(any()))
+				.thenReturn(mock(Mono.class));
+		StepVerifier.create(notificationInquiryService.getAorPracticesByIun("Iun"));
+	}
+	/**
+	 * Method under test: {@link NotificationInquiryService#getAorTransactionByOperationId(String)}
+	 */
+	@Test
+	void testGetAorTransactionByOperationId () {
+
+		when(pnRaddFsuClient.getAorTransactionByOperationId(any())).thenReturn(mock(Mono.class));
+
+		notificationInquiryService.getAorTransactionByOperationId("42");
+
+		verify(pnRaddFsuClient).getAorTransactionByOperationId(any());
+	}
+}
+
